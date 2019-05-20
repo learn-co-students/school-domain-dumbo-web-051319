8333f6a50d82f02eb279a312531a3e2e93b215e4
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-20 21:34:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  
+  attr_accessor :name, :roster
+ 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  
+   def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+  #INSTRUCTIONS FOR add_student 
+  #https://stackoverflow.com/questions/995593/what-does-or-equals-mean-in-ruby
+  
+  def grade(num_grade)
+    roster[num_grade]
+  end
+  
+  def sort 
+    sort = {}
+    roster.each do |grade, students|
+      roster[grade] = students.sort
+    end
+    roster
+  end
+end 
